f48b67dbfb75c18f805ee5df997de532945d2912
Gen4ralz <76762117+Gen4ralz@users.noreply.github.com>
2023-04-05 11:04:51 +0700

    Connect between frontend and backend API

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,36 @@
 import Layout from '../components/Layout'
 import ProductItem from '../components/ProductItem'
-import data from '../utils/data'
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <Layout title="Home Page">
       <div className="grid grid-cols-2 gap-x-2 md:grid-cols-3 lg:grid-cols-4">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <ProductItem product={product} key={product.slug}></ProductItem>
         ))}
       </div>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const headers = new Headers()
+  headers.append('Content-Type', 'application/json')
+
+  const requestOptions = {
+    method: 'GET',
+    headers: headers,
+  }
+
+  const response = await fetch(`http://localhost:8080/products`, requestOptions)
+  const data = await response.json().catch((err) => {
+    console.log(err)
+  })
+  console.log(data)
+
+  return {
+    props: {
+      products: data,
+    },
+  }
 }
